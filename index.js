06fd3e82f5585482d8f48ec0ccbc08b349de20b2
bird2008 <orzechowskib2008@gmail.com>
2022-07-28 13:49:17 +0200

    Add dm notify

--- index.js
+++ index.js
@@ -13,48 +13,51 @@ const client = new Client({
 });
 
 client.on("presenceUpdate", (oldMember, newMember) => {
-		const guild = newMember.guild;
-		member = newMember;
-		if (newMember.user.bot) return;
+	const guild = newMember.guild;
+	member = newMember;
+	if (newMember.user.bot) return;
 
-		activityLength = newMember.member.presence.activities.length;
+	activityLength = newMember.member.presence.activities.length;
 
-		if (activityLength > 0) {
-			console.log("Użytkownicy mają " + activityLength + " aktywność");
+	if (activityLength > 0) {
+		console.log("Użytkownicy mają " + activityLength + " aktywność");
 
-			for (let i = 0; i < activityLength; i++) {
-				console.log(
-					"Aktywność na pozycji " +
-						i +
-						" to " +
-						newMember.member.presence.activities[i].name.toLowerCase()
-				);
-				if (
-					newMember.member.presence.activities[i].name.toLowerCase() ==
-					"league of legends"
-				) {
-					console.log(`${newMember.user.tag} dostał bana za granie w lola.`);
-					try {
-						guild.members
-							.ban(`${newMember.user.id}`, {
-								reason: "Granie w League Of Legends",
-							})
-							.catch((err) => {
-								console.error(err);
-							});
-						break;
-					} catch (err) {}
-				}
+		for (let i = 0; i < activityLength; i++) {
+			console.log(
+				"Aktywność na pozycji " +
+					i +
+					" to " +
+					newMember.member.presence.activities[i].name.toLowerCase()
+			);
+			if (
+				newMember.member.presence.activities[i].name.toLowerCase() ==
+				"league of legends"
+			) {
+				console.log(`${newMember.user.tag} dostał bana za granie w lola.`);
+				try {
+					newMember.user.send(
+						"Nie graj w ligę, ban do odwołania XD - administracja serwera klasowego🙂"
+					);
+					guild.members
+						.ban(`${newMember.user.id}`, {
+							reason: "Granie w League Of Legends",
+						})
+						.catch((err) => {
+							console.error(err);
+						});
+					break;
+				} catch (err) {}
 			}
-		} else {
-			console.log("Brak aktywności");
 		}
-	});
+	} else {
+		console.log("Brak aktywności");
+	}
+});
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
-	
+
 client.on("messageCreate", (message) => {
 	if (message.content === "JD") {
 		message.author.send("JD");
